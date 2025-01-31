641d06bc775395bc74cc0f36b67a661db1b29ad1
BryanChanona <233299@ids.upchiapas.edu.mx>
2025-01-31 01:19:10 -0600

    Nombrando importanción de rutas

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	dependenciesUser "github.com/BryanChanona/arquitectura_hexagonal.git/src/users/infraestructure/dependencies"
-	"github.com/BryanChanona/arquitectura_hexagonal.git/src/users/infraestructure/routes"
+	userRoutesResource "github.com/BryanChanona/arquitectura_hexagonal.git/src/users/infraestructure/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 		dependenciesUser.Init()
 		r := gin.Default()
-		routes.UserRouter(r)
+		userRoutesResource.UserRouter(r)
 		r.Run()
 
 }
