0d4807ddec41eaa4143b211b2c6c72cb4d29494e
BryanChanona <233299@ids.upchiapas.edu.mx>
2025-01-30 18:27:04 -0600

    feat: conectar el recurso users a la base de datos
    - Se implementó la conexión a la base de datos para el recurso users.
    - Se configuró el pool de conexiones con un límite de 10 conexiones abiertas.

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"github.com/BryanChanona/arquitectura_hexagonal.git/src/helpers"
+)
+
+func main() {
+
+	db, err := helpers.ConnectDB()
+
+	if err != nil {
+		log.Fatalf("Error al conectar a la base de datos: %v", err)
+		
+
+	}
+
+	db.Close()
+
+}
